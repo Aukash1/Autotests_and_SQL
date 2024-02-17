79215dc46f00b3c59f8bc4be9f0cd72f772ae6a9
Aukash1 <ksenia.aukash@yandex.ru>
2024-02-17 16:19:24 +0300

    sawdsad

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://688afa37-5df8-42e1-9b77-c9fa85d94684.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Аукаш",
+    "lastName": "Ксения",
+    "address": "NN, 186 apt.",
+    "metroStation": 204,
+    "phone": "+7 920 040 66 09",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-18",
+    "comment": "Ураааа",
+    "color": [
+        "BLACK"
+    ]
+}
